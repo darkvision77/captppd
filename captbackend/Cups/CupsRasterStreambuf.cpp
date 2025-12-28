8571cb52319f3a1fe56d75778d62649edff4370e
Andrey Antufyev <andrey.antufyev@yandex.ru>
2025-12-28 17:28:54 +0300

    Declare hardware margins

--- captbackend/Cups/CupsRasterStreambuf.cpp
+++ captbackend/Cups/CupsRasterStreambuf.cpp
@@ -75,20 +75,31 @@ std::optional<Capt::PageParams> CupsRasterStreambuf::NextPage() {
         throw RasterError("invalid raster format");
     }
     Log::Debug() << "Read header " << header.cupsBytesPerLine << 'x' << header.cupsHeight << " (" << header.cupsPageSizeName << ')';
+    // Margins:
+    //   left  : cupsImagingBBox[0]
+    //   bottom: cupsImagingBBox[1]
+    //   right : cupsPageSize[0] - cupsImagingBBox[2]
+    //   top   : cupsPageSize[1] - cupsImagingBBox[3]
+    uint16_t marginLeft = header.cupsImagingBBox[0] / 72.0f * header.HWResolution[0];
+    uint16_t marginTop = (header.cupsPageSize[1] - header.cupsImagingBBox[3]) / 72.0f * header.HWResolution[0];
+    if (marginTop == 0) {
+        marginTop = 1;
+    }
+    Log::Debug() << "Page margins: left=" << marginLeft << " top=" << marginTop;
     this->linesRemain = header.cupsHeight;
     this->lineBuffer.resize(header.cupsBytesPerLine);
     return Capt::PageParams{
-        .PaperSize = static_cast<uint8_t>(header.cupsInteger[4]),
+        .PaperSize = static_cast<uint8_t>(header.cupsInteger[2]),
         .TonerDensity = static_cast<uint8_t>(header.cupsCompression),
         .Mode = static_cast<uint8_t>(header.cupsMediaType),
         .Resolution = header.HWResolution[0] == 600 ? Capt::ResolutionIdx::RES_600 : Capt::ResolutionIdx::RES_300,
         .SmoothEnable = header.cupsInteger[5] != 0,
         .TonerSaving = header.cupsInteger[6] != 0,
-        .MarginLeft = static_cast<uint16_t>(header.cupsInteger[0]),
-        .MarginTop = static_cast<uint16_t>(header.cupsInteger[1]),
+        .MarginLeft = marginLeft,
+        .MarginTop = marginTop,
         .ImageLineSize = static_cast<uint16_t>(header.cupsBytesPerLine),
         .ImageLines = static_cast<uint16_t>(header.cupsHeight),
-        .PaperWidth = static_cast<uint16_t>(header.cupsInteger[2]),
-        .PaperHeight = static_cast<uint16_t>(header.cupsInteger[3]),
+        .PaperWidth = static_cast<uint16_t>(header.cupsInteger[0]),
+        .PaperHeight = static_cast<uint16_t>(header.cupsInteger[1]),
     };
 }
